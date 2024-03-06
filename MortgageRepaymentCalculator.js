a92977ec0526162befd8ac2c8bed840ee9ac616d
Eray Chumak <eray_chumak@protonmail.com>
2024-03-06 05:39:01 +0000

    Add type definitions

--- MortgageRepaymentCalculator.js
+++ MortgageRepaymentCalculator.js
@@ -33,6 +33,30 @@ const config = {
 // ! for customisation, edit at your own risk
 // ------------------------------------------
 
+// ? Type Definitions - START
+
+/**
+ * @typedef {Object} PaymentMonth
+ *
+ * @property {Number} month - The current month.
+ * @property {Number} time - yyyy-mm-dd for time series.
+ * @property {Number} balance - The remaining mortgage balance.
+ * @property {Number} principal - The principal payment.
+ * @property {Number} interest - The interest payment.
+ * @property {Number} paymentTotal - The principal and interest payment forming the total payment for the month.
+ */
+
+/**
+ * @typedef {Object} PaymentPlan
+ *
+ * @property {Number} startingBalance - The starting mortgage balance.
+ * @property {Number} principalMonthlyPayment - The monthly payments towards the mortgage.
+ * @property {Number} totalInterestPaid - The total fixed interest that will be paid for borrowing.
+ * @property {Array<PaymentMonth>} months - Collection of months spanning the mortgage term.
+ */
+
+// ? Type Definitions - END
+
 const template = document.createElement("template");
 
 template.innerHTML = `
